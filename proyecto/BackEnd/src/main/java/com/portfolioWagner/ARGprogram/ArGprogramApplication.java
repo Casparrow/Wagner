003edd8ce249f955a184db501aeaf3b1c74abada
Casparrow <100289374+Casparrow@users.noreply.github.com>
2022-08-01 11:04:29 -0300

    cvv
    vv

---
+++ proyecto/BackEnd/src/main/java/com/portfolioWagner/ARGprogram/ArGprogramApplication.java
@@ -0,0 +1,13 @@
+package com.portfolioWagner.ARGprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArGprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArGprogramApplication.class, args);
+	}
+
+}
---
+++ proyecto/BackEnd/src/test/java/com/portfolioWagner/ARGprogram/ArGprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioWagner.ARGprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArGprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
